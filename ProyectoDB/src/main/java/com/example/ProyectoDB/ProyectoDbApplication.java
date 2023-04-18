91ae00c400e640776e1d98e12b81592a2fb89f09
Bruno Santos Lanza <bsantosl@emeal.nttdata.com>
2023-04-18 10:02:22 +0200

    cloned, comented and ready to push back

--- ProyectoDB/src/main/java/com/example/ProyectoDB/ProyectoDbApplication.java
+++ ProyectoDB/src/main/java/com/example/ProyectoDB/ProyectoDbApplication.java
@@ -9,5 +9,5 @@ public class ProyectoDbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoDbApplication.class, args);
 	}
-
+	// suma añadida por Br1, comented, ready tu push back.
 }
